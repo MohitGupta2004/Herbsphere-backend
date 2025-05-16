2d1b3835a724b311a8d86801d7e826cff6f308fd
MohitGupta2004 <mohit0907gupta@gmail.com>
2025-05-16 22:57:20 +0530

    updated cors and added system.properties

--- src/main/java/com/project/herbsphere/configuration/CorsConfiguration.java
+++ src/main/java/com/project/herbsphere/configuration/CorsConfiguration.java
@@ -13,10 +13,10 @@ public class CorsConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // Replace with your frontend URL
+                        .allowedOrigins("*") // Replace with your frontend URL
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
+                        .allowedHeaders("*");
+                        //.allowCredentials(true);
             }
         };
     }
